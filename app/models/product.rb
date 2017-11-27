ca3547c0a57f4710237f5dbdbdcbf8306b7c51d7
Ezra <ezra.doucet@gmail.com>
2017-11-27 10:54:45 -0600

    Add search to storefront

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,6 +10,11 @@ class Product < ApplicationRecord
 
   has_attached_file :avatar, styles: {medium: "300x300#", thumb: "100x100#{}"}, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
+  def self.search_by_name_or_description(search_string)
+   where("name LIKE ? OR description LIKE ?", "%#{search_string}%", "%#{search_string}%")
+
+  end
 end
 
 # == Schema Information
