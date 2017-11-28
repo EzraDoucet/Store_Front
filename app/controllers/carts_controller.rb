26b9b4ae37cfa46f1202bd013d6e0c9b45c642c2
Ezra <ezra.doucet@gmail.com>
2017-11-28 10:54:00 -0600

    Add subtotal, destroy, total price methods.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,19 @@
 class CartsController < ApplicationController
+ include CurrentCart
+ before_action :set_cart
+
   def edit
-   include CurrentCart
-   before_action :set_cart
+
+  end
+
+  def destroy
+   if @cart.destroy
+    session[:cart_id] = nil
+    flash[:notice] = "Your Cart Was Successfully Destroyed"
+    redirect_to shop_path
+   else
+    flash[:notice] = "Couldn't Delete Your Cart...Sorry!"
+    redirect_to edit_cart_path
+   end
   end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,16 @@
 class Cart < ApplicationRecord
  has_many :line_items, dependent: :destroy
+
+
+
+ def subtotal
+ #  line_items.to_a.sum { |item| item.total }
+ # ^^ Ruby way to find subtotal - inefficient
+
+  line_items.select("SUM(quantity * price) AS sum")[0].sum
+ end
+
+
 end
 
 # == Schema Information
