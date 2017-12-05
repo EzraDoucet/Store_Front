74ffda23878baeb25fb6f67772782de7018d20e7
Ezra <ezra.doucet@gmail.com>
2017-12-05 10:39:18 -0600

    fix production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_region: ENV['aws_s3_region'],
-    s3_protocol: :'https',
+    s3_protocol: 'https',
     s3_credentials: {
       bucket: ENV['s3_bucket_name'],
       access_key_id: ENV['aws_access_key_id'],
