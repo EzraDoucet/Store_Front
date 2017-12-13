7afdb65b1b1227c7c2ec2261024f4356dc199d61
Ezra <ezra.doucet@gmail.com>
2017-12-13 10:32:34 -0600

    Enable real email messaging.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,20 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true # added
+
+  config.action_mailer.delivery_method = :smtp # added
+
+  # config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: 'localhost:3000',
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    user_name: ENV['gmail_username'],
+    password: ENV['gmail_password']
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
